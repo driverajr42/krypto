fb4f98d5691be43d25db0b562eef39d72358ff04
Dennis <driverajr127@gmail.com>
2022-08-19 17:44:18 -0400

    Installed react-icons, and added images to assets folder

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,10 +4,11 @@ import { Navbar, Welcome, Services, Transactions, Footer } from './components'
 const App = () => {
 
   return (
-    <div>
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
         <Navbar />
         <Welcome />
-
+      </div>
 
       <Services />
       <Transactions />
