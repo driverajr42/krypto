c01818b797262c23cb15ae77e5e96903139ac691
Dennis <driverajr127@gmail.com>
2022-09-03 21:38:52 -0400

    Began Work on Welcome component

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,9 +1,36 @@
+import { AiFillPlayCircle } from 'react-icons/ai'
+import { SiEthereum } from 'react-icons/si'
+import { BsInfoCircle } from 'react-icons/bs'
 
+import { Loader } from '../components'
 
 const Welcome = () => {
+
+  const connectWallet = () => {
+
+  }
+
   return (
-    <div>
-        <h1>Welcome Section</h1>
+    <div className='flex w-full justify-center items-center'>
+        <div className='flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4'>
+
+          <div className='flex flex-1 justify-start flex-col md:mr-10'>
+            <h1 className='text-3xl sm:text-5xl text-white text-gradient py-1'>
+              Send Crypto <br /> accross the world
+            </h1>
+            <p className='text-white text-left mt-5 font-light md:w-9/12 w-11/12 text-base'>
+              Explore the crypto world. Buy and sell cryptocurrencies easily on krypto.
+            </p>
+            <button
+              type='button'
+              onClick={connectWallet}
+              className='flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]'
+            >
+             <p className='text-white text-base font-semibold'>Connect Wallet</p>
+            </button>
+          </div>
+
+        </div>
     </div>
   )
 }
